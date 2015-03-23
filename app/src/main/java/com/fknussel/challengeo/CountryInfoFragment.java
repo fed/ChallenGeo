a54b7672ddf473f9cd1639e9fab0016d967d9748
Federico Knüssel <fknussel@gmail.com>
2015-03-22 23:15:44 -0300

    Display country information

--- app/src/main/java/com/fknussel/challengeo/CountryInfoFragment.java
+++ app/src/main/java/com/fknussel/challengeo/CountryInfoFragment.java
@@ -1,6 +1,7 @@
 package com.fknussel.challengeo;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,11 +11,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import retrofit.Callback;
@@ -40,42 +46,139 @@ public class CountryInfoFragment extends Fragment {
         Intent intent = getActivity().getIntent();
         final String code = intent.getStringExtra("code");
 
-        final TextView codeView = (TextView) rootView.findViewById(R.id.country_code);
-        final TextView nameView = (TextView) rootView.findViewById(R.id.country_name);
-        final TextView populationView = (TextView) rootView.findViewById(R.id.country_population);
-        final TextView regionView = (TextView) rootView.findViewById(R.id.country_region);
-        final TextView subregionView = (TextView) rootView.findViewById(R.id.country_subregion);
+//        final TextView codeView = (TextView) rootView.findViewById(R.id.country_code);
+//        final TextView nameView = (TextView) rootView.findViewById(R.id.country_name);
+//        final TextView populationView = (TextView) rootView.findViewById(R.id.country_population);
         final ImageView flagView = (ImageView) rootView.findViewById(R.id.country_flag);
-        final Button mapView = (Button) rootView.findViewById(R.id.country_map);
+        final ListView infoListView = (ListView) rootView.findViewById(R.id.country_info);
+//        final Button mapView = (Button) rootView.findViewById(R.id.country_map);
 
         ApiClient.getApiInterface().getCountry(code, new Callback<Country>() {
             @Override
             public void success(Country country, Response response) {
-                String name = country.getName();
+                final String name = country.getName();
                 String region = country.getRegion();
                 String subregion = country.getSubregion();
-                
-                final double latitude = country.getLat();
-                final double longitude = country.getLng();
+
+                final double lat = country.getLat();
+                final double lng = country.getLng();
                 
                 double population = country.getPopulation();
                 String formattedPopulation = NumberFormat.getNumberInstance(Locale.US).format(population);
 
-                codeView.setText(code);
-                nameView.setText(name);
+                getActivity().setTitle(name);
+
                 // populationView.setText(String.valueOf((int) population));
-                populationView.setText(formattedPopulation);
-                regionView.setText(region);
-                subregionView.setText(subregion);
+//                populationView.setText(formattedPopulation);
+
+
+                List<String> languages = country.getLanguages();
+                String textLanguages = "";
+                int i = 1;
+                for (String language : languages) {
+                    textLanguages += language;
+                    if (i++ != languages.size()) {
+                        textLanguages += ", ";
+                    }
+                }
+
+                List<String> currencies = country.getCurrencies();
+                String textCurrencies = "";
+                i = 1;
+                for (String currency : currencies) {
+                    textCurrencies += currency;
+                    if (i++ != currencies.size()) {
+                        textCurrencies += ", ";
+                    }
+                }
+
+                List<String> timezones = country.getTimezones();
+                String textTimezones = "";
+                i = 1;
+                for (String timezone : timezones) {
+                    textTimezones += timezone;
+                    if (i++ != timezones.size()) {
+                        textTimezones += ", ";
+                    }
+                }
+
+                List<String> tlds = country.getTopLevelDomain();
+                String textTlds = "";
+                i = 1;
+                for (String tld : tlds) {
+                    textTlds += tld;
+                    if (i++ != tlds.size()) {
+                        textTlds += ", ";
+                    }
+                }
+
+                List<String> callingCodes = country.getCallingCodes();
+                String textCallingCodes = "";
+                i = 1;
+                for (String callingCode : callingCodes) {
+                    textCallingCodes += "+" + callingCode;
+                    if (i++ != callingCodes.size()) {
+                        textCallingCodes += ", ";
+                    }
+                }
                 
-                mapView.setOnClickListener(new View.OnClickListener() {
+                // Construct the data source
+                ArrayList<CountryInfoItem> countryInformation = new ArrayList<>();
+                countryInformation.add(new CountryInfoItem("Region", country.getRegion() + " > " + country.getSubregion()));
+                countryInformation.add(new CountryInfoItem("Population", formattedPopulation));
+                countryInformation.add(new CountryInfoItem("Official Languages", textLanguages));
+                countryInformation.add(new CountryInfoItem("Capital City", country.getCapital()));
+                countryInformation.add(new CountryInfoItem("Currencies", textCurrencies));
+                countryInformation.add(new CountryInfoItem("Timezones", textTimezones));
+                countryInformation.add(new CountryInfoItem("Calling Codes", textCallingCodes));
+                countryInformation.add(new CountryInfoItem("Top Level Domains", textTlds));
+
+                // Create the adapter to convert the array to views
+                CountryInfoAdapter customAdapter = new CountryInfoAdapter(getActivity(), countryInformation);
+
+                // Attach the adapter to a ListView
+                infoListView.setAdapter(customAdapter);
+
+
+                /*mapView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        String uri = String.format(Locale.ENGLISH, "geo:%f,%f", latitude, longitude);
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                        getActivity().startActivity(intent);
+
+                        // Map point based on latitude, longitude and zoom level
+                        String uriBegin = "geo:" + lat + "," + lng ;
+                        String query = lat + "," + lng + "(" + name + ")";
+                        String encodedQuery = Uri.encode(query);
+                        String uriString = uriBegin + "?q=" + encodedQuery + "&z=4";
+                        Uri location = Uri.parse(uriString);
+                        
+                        //Uri location = Uri.parse("geo:" + lat + "," + lng + "?q=" + lat + "," + lng + "(" + name + ")&z=1");
+                        //Uri location = Uri.parse("geo:" + lat + "," + lng + "?z=4&ll=" + lat + "," + lng);
+
+
+//                        Uri location = Uri.parse("geo:" + lat + "," + lng)
+//                                .buildUpon()
+//                                .appendQueryParameter("q", lat + "," + lng + "(" + name + ")")
+//                                .appendQueryParameter("z", "4")
+//                                .build();
+                        
+                        
+                        
+                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+
+                        // Verify There is an App to Receive the Intent
+                        PackageManager packageManager = getActivity().getPackageManager();
+                        List activities = packageManager.queryIntentActivities(mapIntent, PackageManager.MATCH_DEFAULT_ONLY);
+                        boolean isIntentSafe = activities.size() > 0;
+                        
+                        if (isIntentSafe) {
+                            startActivity(mapIntent);
+                        } else {
+                            Toast.makeText(getActivity(), "There isn't any maps application installed", Toast.LENGTH_SHORT).show();
+                        }
+                        
+                        
                     }
-                });
+                }); */
             }
 
             @Override
---
+++ app/src/main/java/com/fknussel/challengeo/CountryInfoItem.java
@@ -0,0 +1,28 @@
+package com.fknussel.challengeo;
+
+public class CountryInfoItem {
+    
+    private String label;
+    private String value;
+
+    public CountryInfoItem(String label, String value) {
+        this.label = label;
+        this.value = value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
